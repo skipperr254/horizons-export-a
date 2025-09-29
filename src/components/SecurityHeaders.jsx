f9c5f009006bc6bb2435a13d93321a39f5a4af16
skipperr254 <skipperr254@gmail.com>
2025-09-30 00:20:37 +0300

    Completely removed helmet to see what happens

--- src/components/SecurityHeaders.jsx
+++ src/components/SecurityHeaders.jsx
@@ -6,7 +6,6 @@ const SecurityHeaders = () => {
     <Helmet>
       <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
-      <meta httpEquiv="X-Frame-Options" content="SAMEORIGIN" />
       <meta httpEquiv="X-XSS-Protection" content="1; mode=block" />
       <meta httpEquiv="Referrer-Policy" content="strict-origin-when-cross-origin" />
     </Helmet>
